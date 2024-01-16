a7568625b11c9a771b8359028fad494319cfd23e
Willami <willami.bezerra@bobby.com.br>
2024-01-15 21:48:58 -0300

    fit: implements design system

--- lib/typography/text_styles.dart
+++ lib/typography/text_styles.dart
@@ -11,4 +11,135 @@ class TextStyles {
     fontWeight: FontWeight.w700,
     height: 0.02,
   );
+  static const heading2 = TextStyle(
+    fontSize: 40,
+    fontWeight: FontWeight.w700,
+    height: 0.03,
+  );
+  static const heading3 = TextStyle(
+    fontSize: 32,
+    fontWeight: FontWeight.w700,
+    height: 0.04,
+  );
+  static const heading4 = TextStyle(
+    fontSize: 24,
+    fontWeight: FontWeight.w700,
+    height: 0.05,
+  );
+  static const heading5 = TextStyle(
+    fontSize: 20,
+    fontWeight: FontWeight.w700,
+    height: 0.06,
+  );
+  static const heading6 = TextStyle(
+    fontSize: 18,
+    fontWeight: FontWeight.w700,
+    height: 0.07,
+  );
+  static const bodyXLargeBold = TextStyle(
+    fontSize: 18,
+    fontWeight: FontWeight.w700,
+    height: 0.08,
+    letterSpacing: 0.20,
+  );
+  static const bodyXLargeSemiBold = TextStyle(
+    fontSize: 18,
+    fontWeight: FontWeight.w600,
+    height: 0.08,
+    letterSpacing: 0.20,
+  );
+  static const bodyXLargeMedium = TextStyle(
+    fontSize: 18,
+    fontWeight: FontWeight.w500,
+    height: 0.08,
+    letterSpacing: 0.20,
+  );
+  static const bodyXLargeRegular = TextStyle(
+    fontSize: 18,
+    fontWeight: FontWeight.w400,
+    height: 0.08,
+    letterSpacing: 0.20,
+  );
+  static const bodyLargeBold = TextStyle(
+    fontSize: 16,
+    fontWeight: FontWeight.w700,
+    height: 0.09,
+  );
+  static const bodyLargeSemiBold = TextStyle(
+    fontSize: 16,
+    fontWeight: FontWeight.w600,
+    height: 0.09,
+  );
+  static const bodyLargeMedium = TextStyle(
+    fontSize: 16,
+    fontWeight: FontWeight.w500,
+    height: 0.09,
+  );
+  static const bodyLargeRegular = TextStyle(
+    fontSize: 16,
+    fontWeight: FontWeight.w400,
+    height: 0.09,
+  );
+  static const bodyMediumBold = TextStyle(
+    fontSize: 14,
+    fontWeight: FontWeight.w700,
+    height: 0.10,
+  );
+  static const bodyMediumSemiBold = TextStyle(
+    fontSize: 14,
+    fontWeight: FontWeight.w600,
+    height: 0.10,
+  );
+  static const bodyMediumMedium = TextStyle(
+    fontSize: 14,
+    fontWeight: FontWeight.w500,
+    height: 0.10,
+  );
+  static const bodyMediumRegular = TextStyle(
+    fontSize: 14,
+    fontWeight: FontWeight.w400,
+    height: 0.10,
+  );
+  static const bodySmallBold = TextStyle(
+    fontSize: 12,
+    fontWeight: FontWeight.w700,
+    height: 0,
+  );
+  static const bodySmallSemiBold = TextStyle(
+    fontSize: 12,
+    fontWeight: FontWeight.w600,
+    height: 0,
+  );
+
+  static const bodySmallMidium = TextStyle(
+    fontSize: 12,
+    fontWeight: FontWeight.w500,
+    height: 0,
+  );
+
+  static const bodySmallRegular = TextStyle(
+    fontSize: 12,
+    fontWeight: FontWeight.w400,
+    height: 0,
+  );
+  static const bodyXSmallBold = TextStyle(
+    fontSize: 10,
+    fontWeight: FontWeight.w700,
+    height: 0,
+  );
+  static const bodyXSmallSemiBold = TextStyle(
+    fontSize: 10,
+    fontWeight: FontWeight.w600,
+    height: 0,
+  );
+  static const bodyXSmallMidium = TextStyle(
+    fontSize: 10,
+    fontWeight: FontWeight.w500,
+    height: 0,
+  );
+  static const bodyXSmallRegular = TextStyle(
+    fontSize: 10,
+    fontWeight: FontWeight.w400,
+    height: 0,
+  );
 }
