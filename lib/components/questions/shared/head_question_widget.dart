a825812cb7053a6abc93e97fdffe55f787d651c6
JosinaldoRocha <josinaldorochaleal@hotmail.com>
2024-02-17 13:53:29 -0300

    feat: I removed the QuestionsPercentIndicatorWidget widget and i deleted the _buildPercentIndicator function

--- lib/components/questions/shared/head_question_widget.dart
+++ lib/components/questions/shared/head_question_widget.dart
@@ -1,7 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:percent_indicator/linear_percent_indicator.dart';
 import 'package:student_design_system/student_design_system.dart';
-import '../../../widgets/percent_indicator/questions_percent_indicator_widget.dart';
 import 'link_to_file_widget.dart';
 
 class HeadQuestionWidget extends StatelessWidget {
@@ -19,7 +17,6 @@ class HeadQuestionWidget extends StatelessWidget {
     return Column(
       crossAxisAlignment: CrossAxisAlignment.start,
       children: [
-        const QuestionsPercentIndicatorWidget(),
         if (title != null) BoxText.heading4(title!),
         const SpaceVertical.x6(),
         Divider(
@@ -42,31 +39,6 @@ class HeadQuestionWidget extends StatelessWidget {
             child: LinkToFileContentWidget(link: file!),
           ),
         if (file != null) const SpaceVertical.x6(),
-      ],
-    );
-  }
-
-  Row _buildPercentIndicator(StudentDSColors dsColor) {
-    return Row(
-      children: [
-        InkWell(
-          onTap: () {},
-          child: Icon(
-            Icons.close_rounded,
-            color: dsColor.dark,
-          ),
-        ),
-        Expanded(
-          child: LinearPercentIndicator(
-            lineHeight: 12,
-            percent: 0.1,
-            backgroundColor: dsColor.dark[100],
-            progressColor: dsColor.primaryPurple,
-            barRadius: const Radius.circular(50),
-            padding: const EdgeInsets.symmetric(horizontal: 30),
-          ),
-        ),
-        BoxText.bodyLargeBold('1/10'),
       ],
     );
   }
