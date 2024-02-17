40ea13d8c13e17702c0ab38c367eff082ea243c4
JosinaldoRocha <josinaldorochaleal@hotmail.com>
2024-02-17 09:04:14 -0300

    feat: I created the dsColor parameter and_buildPercentIndicator function and added the SpaceVertical widget

--- lib/components/questions/shared/head_question_widget.dart
+++ lib/components/questions/shared/head_question_widget.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:percent_indicator/linear_percent_indicator.dart';
 import 'package:student_design_system/student_design_system.dart';
-
 import 'link_to_file_widget.dart';
 
 class HeadQuestionWidget extends StatelessWidget {
@@ -15,9 +15,13 @@ class HeadQuestionWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final dsColor = StudentDesignSystem.config.colors;
+
     return Column(
       crossAxisAlignment: CrossAxisAlignment.start,
       children: [
+        _buildPercentIndicator(dsColor),
+        const SpaceVertical.x10(),
         if (title != null) BoxText.heading4(title!),
         const SpaceVertical.x6(),
         Divider(
@@ -40,6 +44,31 @@ class HeadQuestionWidget extends StatelessWidget {
             child: LinkToFileContentWidget(link: file!),
           ),
         if (file != null) const SpaceVertical.x6(),
+      ],
+    );
+  }
+
+  Row _buildPercentIndicator(StudentDSColors dsColor) {
+    return Row(
+      children: [
+        InkWell(
+          onTap: () {},
+          child: Icon(
+            Icons.close_rounded,
+            color: dsColor.dark,
+          ),
+        ),
+        Expanded(
+          child: LinearPercentIndicator(
+            lineHeight: 12,
+            percent: 0.1,
+            backgroundColor: dsColor.dark[100],
+            progressColor: dsColor.primaryPurple,
+            barRadius: const Radius.circular(50),
+            padding: const EdgeInsets.symmetric(horizontal: 30),
+          ),
+        ),
+        BoxText.bodyLargeBold('1/10'),
       ],
     );
   }
