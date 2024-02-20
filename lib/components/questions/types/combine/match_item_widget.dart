01dc1f5267abe4655797621cad41a879bd340298
JosinaldoRocha <josinaldorochaleal@hotmail.com>
2024-02-20 14:23:14 -0300

    feat: I created the isCorrect, selectedItem and isAnswered parameters and i created the backgroundColor get function

--- lib/components/questions/types/combine/match_item_widget.dart
+++ lib/components/questions/types/combine/match_item_widget.dart
@@ -9,50 +9,61 @@ class MatchItemWidget extends StatelessWidget {
     required this.onTap,
     required this.isSelected,
     required this.isContent,
+    required this.isCorrect,
+    required this.selectdItem,
+    required this.isAnswered,
   }) : super(key: key);
 
   final MatchModel match;
   final Function() onTap;
   final bool isSelected;
   final bool isContent;
+  final bool? isCorrect;
+  final int? selectdItem;
+  final bool isAnswered;
 
   @override
   Widget build(BuildContext context) {
     final dsColor = StudentDesignSystem.config.colors;
 
-    return Padding(
-      padding: const EdgeInsets.only(bottom: 24),
-      child: SizedBox(
-        child: ElevatedButton(
-          onPressed: onTap,
-          style: ElevatedButton.styleFrom(
-            elevation: 0,
-            padding: const EdgeInsets.symmetric(
-              vertical: 12,
-              horizontal: 20,
-            ),
-            backgroundColor: isContent
-                ? isSelected
-                    ? dsColor.primaryPurple
-                    : dsColor.white
-                : isSelected
-                    ? dsColor.secondaryGreen
-                    : dsColor.white,
-            shape: RoundedRectangleBorder(
-              borderRadius: BorderRadius.circular(100),
-              side: isSelected
-                  ? BorderSide.none
-                  : BorderSide(color: dsColor.dark[200]!),
-            ),
-          ),
-          child: Center(
-            child: BoxText.bodyXLargeBold(
-              isContent ? match.content : match.match,
-              color: isSelected ? Colors.white : null,
-            ),
-          ),
+    return ElevatedButton(
+      onPressed: isAnswered ? null : onTap,
+      style: ElevatedButton.styleFrom(
+        elevation: 0,
+        padding: const EdgeInsets.symmetric(
+          vertical: 12,
+          horizontal: 20,
+        ),
+        backgroundColor: backgroundColor,
+        disabledBackgroundColor: dsColor.white,
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(100),
+          side: isSelected
+              ? BorderSide.none
+              : BorderSide(color: dsColor.dark[isAnswered ? 100 : 200]!),
+        ),
+      ),
+      child: Center(
+        child: BoxText.bodyXLargeBold(
+          isContent ? match.content : match.match,
+          color: isAnswered
+              ? dsColor.dark[400]
+              : isSelected
+                  ? Colors.white
+                  : null,
         ),
       ),
     );
+  }
+
+  Color get backgroundColor {
+    final dsColor = StudentDesignSystem.config.colors;
+    if (isCorrect == null) {
+      return isSelected ? dsColor.primaryPurple : dsColor.white;
+    } else if (selectdItem == match.id) {
+      return isCorrect! ? dsColor.secondaryGreen : dsColor.error[500]!;
+    } else {
+      return dsColor.white;
+    }
   }
 }
