5fde6581ce6f67c942c405618e59c3b207c4b8de
JosinaldoRocha <josinaldorochaleal@hotmail.com>
2024-03-06 10:27:34 -0300

    feat: I created the QuizQuestionTypeWidget widget

---
+++ lib/components/questions/types/quiz/quiz_question_type_widget.dart
@@ -0,0 +1,68 @@
+import 'package:flutter/material.dart';
+import 'package:swipe_cards/swipe_cards.dart';
+import '../../models/option_model.dart';
+import 'task_flip_card_type_widget.dart';
+
+class QuizQuestionTypeWidget extends StatefulWidget {
+  const QuizQuestionTypeWidget({
+    Key? key,
+    required this.content,
+    required this.file,
+    required this.expectedAnswer,
+    required this.showLabel,
+    required this.onStackFinished,
+    required this.onAnswer,
+    required this.options,
+    required this.leftSwipeAllowed,
+  }) : super(key: key);
+
+  final String? content;
+  final String? file;
+  final String expectedAnswer;
+  final bool showLabel;
+  final Function() onStackFinished;
+  final Function(SwipeItem, int)? onAnswer;
+  final List<StudentTaskOptionModel> options;
+  final bool leftSwipeAllowed;
+
+  @override
+  State<QuizQuestionTypeWidget> createState() => _QuizQuestionTypeWidgetState();
+}
+
+class _QuizQuestionTypeWidgetState extends State<QuizQuestionTypeWidget> {
+  final List<SwipeItem> _swipeItems = [];
+  MatchEngine? _matchEngine;
+  int currentIndex = -1;
+
+  @override
+  void initState() {
+    for (int i = 0; i < widget.options.length; i++) {
+      _swipeItems.add(
+        SwipeItem(),
+      );
+    }
+    _matchEngine = MatchEngine(swipeItems: _swipeItems);
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return SwipeCards(
+      matchEngine: _matchEngine!,
+      itemBuilder: (BuildContext context, int index) {
+        return TaskFlipCardTypeWidget(
+          content: widget.options[index].content,
+          file: widget.file,
+          expectedAnswer: widget.expectedAnswer,
+          showLabel: widget.showLabel,
+        );
+      },
+      itemChanged: widget.onAnswer,
+      onStackFinished: widget.onStackFinished,
+      leftSwipeAllowed: widget.leftSwipeAllowed,
+      rightSwipeAllowed: true,
+      upSwipeAllowed: false,
+      fillSpace: true,
+    );
+  }
+}
