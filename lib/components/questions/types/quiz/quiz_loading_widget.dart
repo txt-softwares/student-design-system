eb830cb6ac331297ba4623b95579243b230b8d36
Williambz12 <williamsilva754@gmail.com>
2024-03-09 20:45:15 -0300

    fix: quiz loading

--- lib/components/questions/types/quiz/quiz_loading_widget.dart
+++ lib/components/questions/types/quiz/quiz_loading_widget.dart
@@ -25,14 +25,14 @@ class QuizLoadingWidget extends StatelessWidget {
         ),
         const SpaceVertical.x10(),
         const SpaceVertical.x2(),
-        BoxText.heading3('Carregando resultados...'),
+        BoxText.heading3('Carregando resultados'),
         const SpaceVertical.x8(),
         SizedBox(
           height: 8,
           width: 48,
           child: LoadingIndicator(
             indicatorType: Indicator.ballPulse,
-            colors: [StudentDesignSystem.config.colors.primaryPurple],
+            colors: [StudentDesignSystem.config.colors.primaryRed],
             strokeWidth: 1,
           ),
         ),
