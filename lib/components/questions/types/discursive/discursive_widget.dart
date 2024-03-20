1ca0c4323be32e75de282dc44a2bc467a2f349d8
Williambz12 <williamsilva754@gmail.com>
2024-03-20 16:47:53 -0300

    fix: expanded

--- lib/components/questions/types/discursive/discursive_widget.dart
+++ lib/components/questions/types/discursive/discursive_widget.dart
@@ -130,46 +130,44 @@ class _DiscursiveTypeWidgetState extends State<DiscursiveTypeWidget> {
             title: widget.content,
           ),
         ),
-        Expanded(
-          child: Padding(
-            padding: const EdgeInsets.only(bottom: 24),
-            child: Row(
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                Padding(
-                  padding: const EdgeInsets.only(left: 24),
-                  child: Center(
-                    child: SizedBox(
-                      height: 56,
-                      width: 56,
-                      child: SvgPicture.asset(
-                        'assets/images/audio-primary.svg',
-                        package: 'student_design_system',
-                      ),
+        Padding(
+          padding: const EdgeInsets.only(bottom: 24),
+          child: Row(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Padding(
+                padding: const EdgeInsets.only(left: 24),
+                child: Center(
+                  child: SizedBox(
+                    height: 56,
+                    width: 56,
+                    child: SvgPicture.asset(
+                      'assets/images/audio-primary.svg',
+                      package: 'student_design_system',
                     ),
                   ),
                 ),
-                Expanded(
-                  child: Container(
-                    width: double.maxFinite,
-                    margin: const EdgeInsets.all(24)
-                        .copyWith(left: 12, top: 0, bottom: 0),
-                    padding: const EdgeInsets.all(16),
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(
-                        StudentDesignSystem.config.borderRadius,
-                      ),
-                      border: Border.all(
-                        color: StudentDesignSystem.config.colors.dark[200]!,
-                      ),
+              ),
+              Expanded(
+                child: Container(
+                  width: double.maxFinite,
+                  margin: const EdgeInsets.all(24)
+                      .copyWith(left: 12, top: 0, bottom: 0),
+                  padding: const EdgeInsets.all(16),
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(
+                      StudentDesignSystem.config.borderRadius,
                     ),
-                    child: RichText(
-                      text: highlightWords(widget.expectedAnswer, _lastWords),
+                    border: Border.all(
+                      color: StudentDesignSystem.config.colors.dark[200]!,
                     ),
                   ),
+                  child: RichText(
+                    text: highlightWords(widget.expectedAnswer, _lastWords),
+                  ),
                 ),
-              ],
-            ),
+              ),
+            ],
           ),
         ),
         Expanded(
