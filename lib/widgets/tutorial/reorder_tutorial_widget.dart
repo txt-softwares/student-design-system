4cc46945a02133676b1fbd8ff346ac9ee034d82b
JosinaldoRocha <josinaldorochaleal@hotmail.com>
2024-03-27 15:31:39 -0300

    feat: Added the dispose method

--- lib/widgets/tutorial/reorder_tutorial_widget.dart
+++ lib/widgets/tutorial/reorder_tutorial_widget.dart
@@ -30,6 +30,12 @@ class _StudentReorderTutorialWidgetState
   }
 
   @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
     final dsColors = StudentDesignSystem.config.colors;
 
