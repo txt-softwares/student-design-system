e882983f200517e41ff22c9bd8cfeefbdbfb65d1
JosinaldoRocha <josinaldorochaleal@hotmail.com>
2024-04-15 16:08:12 -0300

    feat: I changed the type of the main widget to StatelessWidget and Added the PopScope widget

--- lib/widgets/percent_indicator/questions_percent_indicator_widget.dart
+++ lib/widgets/percent_indicator/questions_percent_indicator_widget.dart
@@ -3,7 +3,7 @@ import 'package:flutter_svg/svg.dart';
 import 'package:percent_indicator/linear_percent_indicator.dart';
 import '../../student_design_system.dart';
 
-class QuestionsPercentIndicatorWidget extends StatefulWidget {
+class QuestionsPercentIndicatorWidget extends StatelessWidget {
   const QuestionsPercentIndicatorWidget({
     super.key,
     required this.totalQuestions,
@@ -13,44 +13,30 @@ class QuestionsPercentIndicatorWidget extends StatefulWidget {
   final int finishedQuestions;
 
   @override
-  State<QuestionsPercentIndicatorWidget> createState() =>
-      _QuestionsPercentIndicatorWidgetState();
-}
-
-class _QuestionsPercentIndicatorWidgetState
-    extends State<QuestionsPercentIndicatorWidget> {
-  bool canPop = false;
-  @override
   Widget build(BuildContext context) {
     final dsColor = StudentDesignSystem.config.colors;
     return PopScope(
-      canPop: canPop,
+      canPop: false,
       onPopInvoked: (value) async {
-        setState(() {
-          canPop = !value;
-        });
-
-        if (canPop) {
-          showModalBottomSheet(
-            context: context,
-            builder: (context) => StudentModalWidget(
-              title: 'Sair da atividade',
-              description: 'Ei! Você vai perder o seu progresso se sair agora.',
-              confirmTitle: 'Continuar',
-              onConfirm: () {
-                setState(() {
-                  canPop = false;
-                });
-                Navigator.pop(context);
-              },
-              cancelTitle: 'Sair',
-              onCancel: () {
-                Navigator.pop(context);
-                Navigator.pop(context);
-              },
-            ),
-          );
+        if (value) {
+          return;
         }
+        showModalBottomSheet(
+          context: context,
+          builder: (context) => StudentModalWidget(
+            title: 'Sair da atividade',
+            description: 'Ei! Você vai perder o seu progresso se sair agora.',
+            confirmTitle: 'Continuar',
+            onConfirm: () {
+              Navigator.pop(context);
+            },
+            cancelTitle: 'Sair',
+            onCancel: () {
+              Navigator.pop(context);
+              Navigator.pop(context);
+            },
+          ),
+        );
       },
       child: Padding(
         padding: const EdgeInsets.only(
@@ -89,15 +75,14 @@ class _QuestionsPercentIndicatorWidgetState
             Expanded(
               child: LinearPercentIndicator(
                 lineHeight: 12,
-                percent: widget.finishedQuestions / widget.totalQuestions,
+                percent: finishedQuestions / totalQuestions,
                 backgroundColor: dsColor.dark[100],
                 progressColor: dsColor.primaryPurple,
                 barRadius: const Radius.circular(50),
                 padding: const EdgeInsets.symmetric(horizontal: 30),
               ),
             ),
-            BoxText.bodyLargeBold(
-                '${widget.finishedQuestions}/${widget.totalQuestions}'),
+            BoxText.bodyLargeBold('$finishedQuestions/$totalQuestions'),
           ],
         ),
       ),
