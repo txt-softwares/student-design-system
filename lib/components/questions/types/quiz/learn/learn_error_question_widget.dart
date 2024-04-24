0bf5d945b96a03364301f4bc48d582d404a677b7
JosinaldoRocha <josinaldorochaleal@hotmail.com>
2024-04-24 11:44:59 -0300

    feat: Added the ClipRRect and CachedNetworkImage widgets

--- lib/components/questions/types/quiz/learn/learn_error_question_widget.dart
+++ lib/components/questions/types/quiz/learn/learn_error_question_widget.dart
@@ -1,3 +1,4 @@
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_widget_from_html_core/flutter_widget_from_html_core.dart';
 import 'package:student_design_system/student_design_system.dart';
@@ -19,12 +20,17 @@ class LearnErrorQuestionWidget extends StatelessWidget {
         color: StudentDesignSystem.config.colors.error[50]!,
       ),
       padding: const EdgeInsets.all(24),
-      child: HtmlWidget(
-        '<center>${item.expectedAnswer}</center>',
-        textStyle: TextStyles.bodyXLargeSemiBold.copyWith(
-          color: StudentDesignSystem.config.colors.error,
-        ),
-      ),
+      child: item.file != null
+          ? ClipRRect(
+              borderRadius: BorderRadius.circular(8),
+              child: CachedNetworkImage(imageUrl: item.file!),
+            )
+          : HtmlWidget(
+              '<center>${item.content}</center>',
+              textStyle: TextStyles.bodyXLargeSemiBold.copyWith(
+                color: StudentDesignSystem.config.colors.error,
+              ),
+            ),
     );
   }
 }
