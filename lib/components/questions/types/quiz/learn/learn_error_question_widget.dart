e783733831f2e3d6710856ee302b78e5f06f8b2a
JosinaldoRocha <josinaldorochaleal@hotmail.com>
2024-04-25 08:39:57 -0300

    feat: i changed the type of the main widget to StatefulWidget, i called the initState and dispose methods, i created the tts paramete and added the Stack, Align, GestureDetector and Icon widgets

--- lib/components/questions/types/quiz/learn/learn_error_question_widget.dart
+++ lib/components/questions/types/quiz/learn/learn_error_question_widget.dart
@@ -3,13 +3,35 @@ import 'package:flutter/material.dart';
 import 'package:flutter_widget_from_html_core/flutter_widget_from_html_core.dart';
 import 'package:student_design_system/student_design_system.dart';
 
-class LearnErrorQuestionWidget extends StatelessWidget {
+import '../../../../../utils/text_to_speech.dart';
+
+class LearnErrorQuestionWidget extends StatefulWidget {
   const LearnErrorQuestionWidget({
     super.key,
     required this.item,
   });
 
   final QuizQuestionModel item;
+
+  @override
+  State<LearnErrorQuestionWidget> createState() =>
+      _LearnErrorQuestionWidgetState();
+}
+
+class _LearnErrorQuestionWidgetState extends State<LearnErrorQuestionWidget> {
+  final tts = StudentTTS();
+
+  @override
+  void initState() {
+    super.initState();
+    tts.init();
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    tts.stop();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -20,17 +42,30 @@ class LearnErrorQuestionWidget extends StatelessWidget {
         color: StudentDesignSystem.config.colors.error[50]!,
       ),
       padding: const EdgeInsets.all(24),
-      child: item.file != null
-          ? ClipRRect(
-              borderRadius: BorderRadius.circular(8),
-              child: CachedNetworkImage(imageUrl: item.file!),
-            )
-          : HtmlWidget(
-              '<center>${item.content}</center>',
-              textStyle: TextStyles.bodyXLargeSemiBold.copyWith(
-                color: StudentDesignSystem.config.colors.error,
-              ),
+      child: Stack(
+        children: [
+          widget.item.file != null
+              ? ClipRRect(
+                  borderRadius: BorderRadius.circular(8),
+                  child: CachedNetworkImage(imageUrl: widget.item.file!),
+                )
+              : HtmlWidget(
+                  '<center>${widget.item.content}</center>',
+                  textStyle: TextStyles.bodyXLargeSemiBold.copyWith(
+                    color: StudentDesignSystem.config.colors.error,
+                  ),
+                ),
+          Align(
+            alignment: Alignment.topRight,
+            child: GestureDetector(
+              onTap: () {
+                tts.speak(widget.item.expectedAnswer);
+              },
+              child: const Icon(IconlyBold.volumeUp),
             ),
+          ),
+        ],
+      ),
     );
   }
 }
